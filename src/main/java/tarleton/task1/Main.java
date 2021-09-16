afbb07fde011391ed4164785705cd73460eefafd
Joe Meier <joseph.meier@go.tarleton.edu>
2021-09-16 08:06:01 -0500

    initial commit

---
+++ src/main/java/tarleton/task1/Main.java
@@ -0,0 +1,14 @@
+
+
+package tarleton.task1;
+
+/**
+ *
+ * @author Joe Meier
+ */
+public class Main {
+    public static void main(String[] args){
+        //Adding comments
+        System.out.println("hi");
+    }
+}
